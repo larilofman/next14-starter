2aaf0e6811e89e3e6bba372ef4fa6a07fffd81c9
Lari Löfman <larwazor@gmail.com>
2024-03-23 17:07:39 +0200

    add pages

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import Links from "./links/Links"
+
+const NavBar = () => {
+  return (
+    <div>
+      <div>Logo</div>
+      <div>
+        <Links></Links>
+      </div>
+    </div>
+  )
+}
+
+export default NavBar;
