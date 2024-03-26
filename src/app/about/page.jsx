45f604431021703ccdd656d7e7134fda0f5349ec
Lari Löfman <larwazor@gmail.com>
2024-03-27 00:45:31 +0200

    add images

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,6 +1,14 @@
+import Image from "next/image";
+import styles from "./about.module.css"
+
 const AboutPage = () => {
   return (
-    <div>AboutPage</div>
+    <div>
+      <div className={styles.imgContainer}>
+        <Image src="/about.png" alt="" fill />
+        {/* <Image src="https://images.pexels.com/photos/17528288/pexels-photo-17528288/free-photo-of-jaguar-lying-on-tree-log.jpeg" alt="" fill /> */}
+      </div>
+    </div>
   )
 }
 
