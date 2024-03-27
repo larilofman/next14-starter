e782c288d9c3e95d6e46ccbe277476a9d8e82860
Lari Löfman <larwazor@gmail.com>
2024-03-28 00:18:26 +0200

    add about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,10 +3,37 @@ import styles from "./about.module.css"
 
 const AboutPage = () => {
   return (
-    <div>
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h2 className={styles.subtitle}>About Agency</h2>
+        <h1 className={styles.title}>We create digital ideas that are bigger, bolder, braver and better.</h1>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Minus et deleniti libero nihil quod expedita aliquam unde fuga sit,
+          nostrum cumque deserunt iusto sint harum error excepturi! Error, nobis neque.
+        </p>
+        <div className={styles.boxes}>
+          <div className={styles.box}>
+            <h1>10+</h1>
+            <p>Years of experience</p>
+          </div>
+          <div className={styles.box}>
+            <h1>123 K+</h1>
+            <p>People reached</p>
+          </div>
+          <div className={styles.box}>
+            <h1>5 K+</h1>
+            <p>Services and plugins</p>
+          </div>
+        </div>
+      </div>
       <div className={styles.imgContainer}>
-        <Image src="/about.png" alt="" fill />
-        {/* <Image src="https://images.pexels.com/photos/17528288/pexels-photo-17528288/free-photo-of-jaguar-lying-on-tree-log.jpeg" alt="" fill /> */}
+        <Image
+          src="/about.png"
+          alt="About Image"
+          fill
+          className={styles.img}
+        />
       </div>
     </div>
   )
