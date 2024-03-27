6f4ee8409e58e04494b686e25ff18a5630fa5641
Lari Löfman <larwazor@gmail.com>
2024-03-27 17:04:35 +0200

    add homepage styling

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,11 @@
+import styles from "./footer.module.css"
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className={styles.container}>
+      <div className={styles.logo}>Kokodev</div>
+      <div className={styles.text}>Koko creative thoughts agency © All rights reserved.</div>
+    </div>
   )
 }
 
